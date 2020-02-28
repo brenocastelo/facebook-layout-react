8e5bbad2bc25be875c92d12efb7ba9789c3c34af
Breno Castelo Branco <breno@vizir.com.br>
2020-02-28 15:12:00 -0300

    feat: config webpack and babel

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './App.css'
+
+function App() {
+  return (
+    <h1>Facebook</h1>
+  )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    path: path.resolve(__dirname, 'src', 'index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: {
+          loader: 'file-loader'
+        }
+      }
+    ]
+  }
+}
